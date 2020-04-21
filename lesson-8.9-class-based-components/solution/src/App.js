e40399ee1fa9567ec348e9d4c77be3e3a49f2557
Keyairra Wright <keyairra.wright@gmail.com>
2020-04-20 22:19:09 -0400

    Create FunctionBasedForm solution outline and edit css

--- lesson-8.9-class-based-components/solution/src/App.js
+++ lesson-8.9-class-based-components/solution/src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react'
-import { Container } from 'reactstrap'
-import ClassBasedForm from './components/ClassBasedForm'
-import Logo from './assets/alligator-logo2.svg'
-import './App.css'
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+// import ClassBasedForm from './components/ClassBasedForm'
+import FunctionBasedForm from './components/FunctionBasedForm';
+import Logo from './assets/marcy_lab_log.jpg';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <img src={ Logo } alt="alligator.io logo" width="200" />
-        <Container>
-          <ClassBasedForm />
-        </Container>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<img src={Logo} alt="marcy lab log" width="200" />
+				<Container>
+					<FunctionBasedForm />
+				</Container>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
---
+++ lesson-8.9-class-based-components/solution/src/components/FunctionBasedForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FunctionBasedForm = () => {
+	return <h1>Function Based Form</h1>;
+};
+
+export default FunctionBasedForm;

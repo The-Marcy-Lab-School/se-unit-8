ce2e761e53a92c0a68b18db6576b8447fc6f8644
Reuben Ogbonna <reubenogbonna@gmail.com>
2020-04-22 13:17:13 -0400

    moves problem set to its own repo

--- problem-set-1-tic-tac-toe/tic-tac-toe-app/App.jsx
+++
@@ -1,5 +0,0 @@
-const App = () => {
-	return <div>A tic-tac-toe game</div>;
-};
-
-ReactDOM.render(<App />, document.querySelector('.container'));
--- problem-set-1-tic-tac-toe/tic-tac-toe-app/README.md
+++
@@ -1,6 +0,0 @@
-# React-Tac-Toe w/ Hooks
-An exploration of the Hook
-
-## Instructions
-1. Run `npm start`
-2. View on `http://localhost:5000`
